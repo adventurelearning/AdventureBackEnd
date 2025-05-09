4f3821068d3f6499fad865482179ee124f503b81
Adventureexamrally <adventure.webapp@gmail.com>
2025-05-09 17:48:05 +0530

    Update intern model

--- models/InternRegister.js
+++ models/InternRegister.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const formDataSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  contactNumber: { type: String, required: true },
-  email: { type: String, required: true },
-  collegeName: { type: String, required: true },
-  degree: { type: String, required: true },
-  mainCourse: { type: String, required: true },
-  internshipDomain: { type: String, required: true },
-  additionalComments: { type: String },
-  resumeUrl: { type: String, required: true },
-}, { timestamps: true });
+const formDataSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    contactNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    collegeName: { type: String, required: true },
+    degree: { type: String, required: true },
+    mainCourse: { type: String, required: true },
+    internshipDomain: { type: String, required: true },
+    mode: { type: String, required: true },
+    additionalComments: { type: String },
+    resumeUrl: { type: String,},
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('intern', formDataSchema);
+module.exports = mongoose.model("intern", formDataSchema);
